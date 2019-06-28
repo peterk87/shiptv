056aa64d525d9dbca1ebf7155630f43ab8fa874c
Peter Kruczkiewicz <peter.kruczkiewicz@gmail.com>
2019-06-28 11:49:37 -0500

    Add feature #2, add option to collapse low support branches and output modified Newick file

--- tests/test_shiptv.py
+++ tests/test_shiptv.py
@@ -9,6 +9,8 @@ from os.path import abspath, exists
 from pandas.testing import assert_frame_equal
 
 from shiptv import cli
+from shiptv.shiptv import collapse_branches
+from ete3 import Tree
 
 input_ref_genbank = abspath('tests/data/fmdv-5.gb')
 input_newick = abspath('tests/data/fmdv-5.newick')
@@ -27,11 +29,58 @@ def test_command_line_interface():
     with runner.isolated_filesystem():
         out_html = 'test.html'
         out_table = 'test.tsv'
+        out_newick = 'test.newick'
         test_result = runner.invoke(cli.main, ['-r', input_ref_genbank,
                                                '-n', input_newick,
+                                               '-N', out_newick,
                                                '-o', out_html,
                                                '-m', out_table])
         assert test_result.exit_code == 0
         assert exists(out_html)
         assert exists(out_table)
+        assert exists(out_newick)
+        assert open(input_newick).read() != open(out_newick).read()
         assert_frame_equal(pd.read_csv(expected_table, sep='\t'), pd.read_csv(out_table, sep='\t'))
+
+    with runner.isolated_filesystem():
+        out_html = 'test.html'
+        out_table = 'test.tsv'
+        test_result = runner.invoke(cli.main, ['-r', input_ref_genbank,
+                                               '-n', input_newick,
+                                               '-o', out_html,
+                                               '-m', out_table,
+                                               '-C', 95])
+        assert test_result.exit_code == 0
+        assert exists(out_html)
+        assert exists(out_table)
+        assert_frame_equal(pd.read_csv(expected_table, sep='\t'), pd.read_csv(out_table, sep='\t'))
+
+
+def test_collapse_branches():
+    before_tree_ascii = """
+   /-MK088171.1
+  |
+--|--MK071699.1
+  |
+  |   /-MH845413.2
+   \-|
+     |   /-MH784405.1
+      \-|
+         \-MH784404.1
+    """.strip()
+    after_tree_ascii = """    
+   /-MK088171.1
+  |
+  |--MK071699.1
+--|
+  |--MH845413.2
+  |
+  |   /-MH784405.1
+   \-|
+      \-MH784404.1
+    """.strip()
+
+    tree = Tree(newick=input_newick)
+    assert tree.get_ascii().strip() == before_tree_ascii
+    collapse_branches(tree, 95)
+    assert tree.get_ascii().strip() == after_tree_ascii
