236a81e7fc009fe2f8315b3ec6272552c526db65
Peter Kruczkiewicz <peter.kruczkiewicz@gmail.com>
2019-06-28 10:49:51 -0500

    Fix #1, date/time parsing exception

--- tests/test_shiptv.py
+++ tests/test_shiptv.py
@@ -6,9 +6,10 @@
 import pandas as pd
 from click.testing import CliRunner
 from os.path import abspath, exists
-from pandas.testing import assert_frame_equal
+from pandas.testing import assert_frame_equal, assert_series_equal
 
 from shiptv import cli
+from shiptv.shiptv import fix_collection_date
 
 input_ref_genbank = abspath('tests/data/fmdv-5.gb')
 input_newick = abspath('tests/data/fmdv-5.newick')
@@ -35,3 +36,9 @@ def test_command_line_interface():
         assert exists(out_html)
         assert exists(out_table)
         assert_frame_equal(pd.read_csv(expected_table, sep='\t'), pd.read_csv(out_table, sep='\t'))
+
+def test_fix_collection_date():
+    df = pd.DataFrame(dict(collection_date=['1994/1995', '2000', 'not-a-date', '2009/03/31']))
+    fix_collection_date(df)
+    expected_years = pd.Series([None, 2000, None, 2009], name='collection_year')
+    assert_series_equal(df.collection_year, expected_years)
